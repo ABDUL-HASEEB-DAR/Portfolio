d834e810ddb7441d758ba3232d4ebbf1bb8a3076
Abdul Haseeb Dar <haseebdar696@gmail.com>
2024-06-26 16:36:45 +0500

    css and nodemailer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {dirname} from 'path';
 import {fileURLToPath} from 'url';
+import nodemailer from 'nodemailer';
 
 const port = 3000;
 const app = express();
@@ -12,7 +13,55 @@ app.use(express.static('./public'));
 
 app.get('/',(req, res)=>{
     res.sendFile(__dirname + '/public/html/index.html');
-    
+})
+
+app.post('/',(req,res)=>{
+    const fName = req.body.firstName;
+    const lName = req.body.lastName;
+    const email = req.body.email;
+    const comment = req.body.message;
+
+    /////////////////// Node Mailer /////////////////////
+
+    const emailContent = `
+    <h2><strong>Name:</strong> ${fName +" "+lName}</h2>
+    <h3><strong>Email:</strong> ${email}</h3>
+    <p><strong>Message:</strong> ${comment}</p>`;
+  // Define email configuration
+  let config = {
+    service: "gmail",
+    auth: {
+      user: "newbooking45@gmail.com",
+      pass: "dpqc hkev uupm kgjg",
+    },
+  };
+
+  // Create transporter
+  let transporter = nodemailer.createTransport(config);
+
+  // Define email message
+  let message = {
+    from: "newbooking45@gmail.com",
+    to: "haseebdar696@gmail.com",
+    subject: "Portfolio Message",
+    html: emailContent,
+  };
+
+  // Send email
+  transporter
+    .sendMail(message)
+    .then((info) => {
+      console.log("Email sent:", info.messageId);
+      console.log("Preview URL:", nodemailer.getTestMessageUrl(info));
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.error("Error sending email:", err);
+      res.status(500).json({ error: "Failed to send email" });
+    });
+    ////////////////////////////////////////////////////////////////////////
+
+    res.redirect('/');
 })
 
 app.listen( port || process.env.PORT, () => {
---
+++ public/js/file.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.autoType', {
+  strings: ["Electrical Engineer", "Web Developer", "App Developer", "Pixel Artist"],
+  typeSpeed: 50,
+  loop: true,
+  backSpeed: 50,
+  backDelay: 1000
+});
